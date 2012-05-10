f90db5082ed0aca533b71b9bb457b0708c32388c
TaurusOlson <taurusolson@gmail.com>
2012-05-10 23:56:08 +0200

    Add message for the tests

--- nanotest.py
+++ nanotest.py
@@ -3,51 +3,61 @@
 
 
 class Nanotest(object):
-    """docstring for Nanotest"""
-
-    def __init__(self, arg, is_true=True):
+    def __init__(self, arg):
         self.arg = arg
-        self.is_true = is_true
 
     def comparison(self, other, relation):
         if isinstance(self.arg, int):
             result = getattr(float(self.arg), relation)(other)
         else:
             result = getattr(self.arg, relation)(other)
-
-        if self.is_true:
-            return result
-        else:
-            return not result
+        return result
 
     def __eq__(self, other):
         return self.comparison(other, '__eq__')
 
     def eq(self, other):
+        print "be equal to %r..." % other
         return self.comparison(other, '__eq__')
 
     def ne(self, other):
+        print "not be equal to %r..." % other
         return self.comparison(other, '__ne__')
 
     def gt(self, other):
+        print "be greater than %r..." % other
         return self.comparison(other, '__gt__')
 
     def ge(self, other):
+        print "be greater or equal to %r..." % other
         return self.comparison(other, '__ge__')
 
     def lt(self, other):
+        print "be lower than %r..." % other
         return self.comparison(other, '__lt__')
 
     def le(self, other):
+        print "be lower or equal to %r..." % other
         return self.comparison(other, '__le__')
 
+    def is_(self, other):
+        print "be %r" % other
+        return self.arg is other
 
-def should_be(a):
-    return Nanotest(a, True)
+    def is_not(self, other):
+        print "not be %r..." % other
+        return self.arg is not other
 
-def should_not_be(a):
-    return Nanotest(a, False)
+    def contains(self, other):
+        print "contain %r..." % other
+        return other in self.arg 
+
+    def contains_no(self, other):
+        print "not contain %r..." % other
+        return other not in self.arg
+
+def test(a):
+    print "%r should" % a,
+    return Nanotest(a)
 
 
-
-       
